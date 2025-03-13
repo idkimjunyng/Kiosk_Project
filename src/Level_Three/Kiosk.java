1ffb4227c7028b04e73a70be639948ad8b56bffe
idkimjunyng <kimjunyng@icloud.com>
2025-03-13 16:29:02 +0900

    레벨1 브랜치 초기 코드 커밋
    레벨1 브랜치 초기 코드 커밋

---
+++ src/Level_Three/Kiosk.java
@@ -0,0 +1,62 @@
+package Level_Three;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+
+    List<MenuItem> menuItemList = new ArrayList<>();
+
+    public Kiosk() {
+        this.menuItemList.add(new MenuItem(1,"ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        this.menuItemList.add(new MenuItem(2,"SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        this.menuItemList.add(new MenuItem(3,"CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        this.menuItemList.add(new MenuItem(4,"Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isRunning = true;
+
+        while(isRunning) {
+
+            System.out.println("[ SHAKE SHAKE MENU ]");
+
+            for (int i = 0; i < menuItemList.size(); i++) {
+
+                MenuItem menuList = (MenuItem) menuItemList.get(i);
+
+                System.out.println(menuList.getIndex() + ". " + menuList.getName() + "                | W " + menuList.getPrice() + " | " + menuList.getDescription());
+            }
+            System.out.println("0. 종료");
+
+            int shakeShakeIndexNumber = scanner.nextInt();
+            if (shakeShakeIndexNumber == 1) {
+                MenuItem menuList = (MenuItem) menuItemList.get(0);
+                System.out.println(menuList.getIndex() + ". " + menuList.getName() + "                | W " + menuList.getPrice() + " | " + menuList.getDescription());
+            } else if (shakeShakeIndexNumber == 2) {
+                MenuItem menuList = (MenuItem) menuItemList.get(1);
+                System.out.println(menuList.getIndex() + ". " + menuList.getName() + "                | W " + menuList.getPrice() + " | " + menuList.getDescription());
+            } else if (shakeShakeIndexNumber == 3) {
+                MenuItem menuList = (MenuItem) menuItemList.get(2);
+                System.out.println(menuList.getIndex() + ". " + menuList.getName() + "                | W " + menuList.getPrice() + " | " + menuList.getDescription());
+            } else if (shakeShakeIndexNumber == 4) {
+                MenuItem menuList = (MenuItem) menuItemList.get(3);
+                System.out.println(menuList.getIndex() + ". " + menuList.getName() + "                | W " + menuList.getPrice() + " | " + menuList.getDescription());
+            } else if (shakeShakeIndexNumber == 0) {
+                System.out.println("키오스크가 종료되었습니다.");
+                isRunning = false;
+            } else {
+                System.out.println("메뉴를 확인해주세요.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Kiosk kiosk = new Kiosk();
+        kiosk.start();
+    }
+
+}
